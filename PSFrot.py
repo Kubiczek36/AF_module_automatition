f79f2cf07a757de8219fc967fb3927633df6c033
Kubiczek36 <kubadokulil99@gmail.com>
2022-01-18 12:50:13 +0100

    need to find a better way to recognize the circles

--- PSFrot.py
+++ PSFrot.py
@@ -1,22 +1,25 @@
 class PSFrot(object):
     import numpy as np
-    def readImage(path, gray = True, normalize=True):
+
+    def readImage(path, gray=True, normalize=True):
         """
         erads image from a file
         """
         from skimage import exposure
-        from skimage import io  
+        from skimage import io
         im = io.imread(path, as_gray=gray, plugin='pil')
         if normalize:
             im = exposure.rescale_intensity(im)
         return im
+
     def threshold(im, intensity=0.4):
         """
         Makes an threshold on given intensity level.
         """
         thresholded = (im < intensity)*1.0
         return thresholded
-    def findCircles(im , minRadius = 2, maxRadius = 5):
+
+    def findCircles(im, minRadius=2, maxRadius=5):
         """
         finds circler in a image
         returns arrays of centres
@@ -24,12 +27,14 @@ class PSFrot(object):
         from skimage.transform import hough_circle, hough_circle_peaks
         hough_radii = range(minRadius, maxRadius)
         hough_res = hough_circle(im, hough_radii)
-        accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii, total_num_peaks=2)
+        accums, cx, cy, radii = hough_circle_peaks(
+            hough_res, hough_radii, total_num_peaks=2)
         return cx, cy, radii
 
 # %% Libraries import
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     from PIL import Image
     from PIL import ImageOps
     from skimage import exposure
@@ -43,15 +48,15 @@ if __name__=="__main__":
 
     def imShow2(im1, im2, cmap='Greys'):
         fig, axs = plt.subplots(2)
-        axs[0].imshow(im1, cmap = cmap)
-        axs[1].imshow(im2, cmap = cmap)
+        axs[0].imshow(im1, cmap=cmap)
+        axs[1].imshow(im2, cmap=cmap)
         plt.show()
 
-    def normalize(imAr, type = 255):
+    def normalize(imAr, type=255):
         """
         `imAr` array of images
         """
-        return(np.uint8(imAr* 255/np.max(imAr)))
+        return(np.uint8(imAr * 255/np.max(imAr)))
 
     # %% import test image
     imC = Image.open("test_imgs/0.tiff").convert('L')
@@ -60,21 +65,21 @@ if __name__=="__main__":
 
     imShow2(imAr, normalize(imAr))
 
-
     # %%
     im = io.imread("test_imgs/3.tiff", as_gray=True)
-    im = exposure.rescale_intensity(im) # normalizace obrázku
+    im = exposure.rescale_intensity(im)  # normalizace obrázku
 
-    binGray = (im>0.4)*1.
+    binGray = (im > 0.4)*1.
     plt.imshow(binGray)
     plt.colorbar()
 
     # https://www.geeksforgeeks.org/image-segmentation-using-pythons-scikit-image-module/
 
-    # %% segmentation 
+    # %% segmentation
     hough_radii = np.arange(2, 5)
     hough_res = hough_circle(binGray, hough_radii)
-    accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii, total_num_peaks=2)
+    accums, cx, cy, radii = hough_circle_peaks(
+        hough_res, hough_radii, total_num_peaks=2)
 
     # %%
 
